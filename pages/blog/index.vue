9ca4a27cbd87f79a14e4f1b3eee080e7218ecfc2
Angus Gaukroger <angusgaukroger@looptechnology.com>
2023-03-20 11:46:17 +0000

    overflow fixed finally

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-screen w-screen flex flex-col justify-center items-center ">
+    <div class="h-screen w-full flex flex-col justify-center items-center ">
         Hello Bloggers
     </div>
 </template>
