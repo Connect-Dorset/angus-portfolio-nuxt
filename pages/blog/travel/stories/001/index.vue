99228aaecae26a4869cec0d61ce0c643ecb7e964
Angus Gaukroger <angusgaukroger@looptechnology.com>
2023-03-21 15:04:30 +0000

    Added blog page and tried to embed tiktok

---
+++ pages/blog/travel/stories/001/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="top-0 relative w-full h-auto flex flex-col justify-center items-center pt-24 px-20 gap-3">
+        <h1 class="text-4xl">I solo travelled South America</h1>
+        <p>I’ll never forget how it feels. The moment you book the flight to take on an adventure that you’ve always dreamed of, completely alone. There is nothing like the sinking feeling of knowing you’ve plunged yourself in at the deep end, and now you’ve got to get on with it. 
+        </p>
+        <div class="flex flex-row justify-evenly items-center w-full">
+            <p>
+                Sometime in early February of 2022 I pulled the trigger and booked my flight from London Heathrow to Cartagena, Colombia on the 31st of May 2022. I’d been wanting to do this for so long, and the monotony of working an office job, alongside never fully getting back to the “highs” of student life since finishing the previous year had made me desperate to take on an adventure. The COVID pandemic was coming to an end, and countries were opening up their borders. There were high amounts of uncertainty among travellers about where and how to travel in this new age, but I knew that I couldn’t wait a single moment longer.
+            </p>
+            <iframe src="https://www.tiktok.com/@angus_gaukroger/video/7104693402884754694?is_from_webapp=1&sender_device=pc&web_id=7195210233014175238"/>
+        </div>
+        
+        
+        <p>
+            Since my first year of university, I had wanted to see what South America was like, and since April 2020 I had been consistently practising and learning Spanish, to the point I could now speak at a pretty good level. Initially, there had been discussions had been made with different friends through uni about who would join me on the trip, but ultimately it was evident that no one was as serious about this as me. So I would have to go alone, this would turn out to be the greatest decision of my life so far.
+
+        </p>
+        <p>
+            As soon as I had finished buying the tickets my head dropped into my hands as I processed the decision I had just made. I waited a while before going downstairs to tell my mum what I had done. She looked at me and started to cry, not the reaction I had anticipated.
+        </p>
+        <p>
+            The months that followed this moment were a blur. I was granted sabbatical leave by my manager which gave me a 4-month window to travel (I had planned to quit my job if this leave was not granted. Luckily, it was!). During this time I  got vaccines, insurance, bags, clothes etc to prepare myself for the journey of a lifetime. My route was planned and as the 31st of May rolled around I was buzzing with excitement.
+        </p>
+        <p>
+            Let me tell you, stepping out of my parent’s car at Heathrow airport was one of the most surreal and emotional experiences of my life. Anyone that downplays what this is like clearly has no idea. I said my goodbyes to my brother and my parents and made my way towards the terminal, passport in hand. I would consider myself to be very close to my family and I almost felt selfish to be doing this by myself. But part of growing is listening to what calls you, and doing what you feel you need to do. It’s in these moments that we feel most alive.
+        </p>
+        <p>
+            Some 15 hours of travelling later, I touched down in Cartagena, Colombia. As I stepped off the plane, a wave of heat hit me - the humidity of the Carribean!  Colombia is the most diverse country on the planet, and the contrast between the cold morning in Bogotá to this was a shock to the whole system. My entire body immediately started sweating, which would be an ongoing theme in this area of the country!
+        </p>
+        <p>
+            I left the airport, jumped in a taxi and headed to the hostel I had booked. My initial impressions of Cartagena were that it was rough around the edges, but very colourful. The sun was shining and everyone seemed to be in a rush to get somewhere. I had heard that Cartagena was quite the tourist trap for gringos, and this could not be any more accurate. Around every corner throughout my stay in this city, people were trying to sell me something on every corner.
+        </p>
+        <p>
+            The usual dialogue with the vendors went something like this:
+
+            Vendor (Broken English accent): Hey man! You want some water?
+            Me: No gracias.
+            Vendor: Oh okay man… weed? Cocaine?
+            Me: No gracias.
+
+            I would estimate that this happened at least once every 2 minutes while moving around the historic centre and Getsemani.
+        </p>
+        <p>
+            As I arrived at my hostel I was dying to get some rest, it was 8am and I had slept really badly on the flight. I asked to check in early and was told this was not possible until at least after midday. This sucked, but it was okay… Part of the experience! I lazed around the pool for the morning and eventually took a nap/organised my things when I could finally get to my room in the afternoon.
+
+        </p>
+    </div>
+</template>
+
+<!-- <script setup>
+
+</script> -->
+<style scoped>
+
+</style>
---
+++ pages/blog/travel/stories/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-full h-screen flex flex-col justify-center items-center">
+       
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
