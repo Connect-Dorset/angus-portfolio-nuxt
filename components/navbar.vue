60fe04ca36c22c44b7791d40b202c5ea0b854be7
Angus Gaukroger <angusgaukroger@looptechnology.com>
2023-03-22 10:39:36 +0000

    blog dropdown (layer 1)

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="w-full overflow-x-hidden z-[100] top-0 fixed h-20 py-3 px-6 bg-gradient-to-tr from-mine-shaft-600 to-mine-shaft-500 text-soft-peach-100 flex flex-row justify-around items-center">
+    <nav class="w-full overflow-x-hidden z-[99] top-0 fixed h-20 py-3 px-6 bg-gradient-to-tr from-mine-shaft-600 to-mine-shaft-500 text-soft-peach-100 flex flex-row justify-around items-center">
                 <div class="flex flex-col justify-center items-center h-full">
-                    <h2 class="font-semibold text-xl">
-                        <NuxtLink to="/">
+                    <h2 class="font-semibold text-xl " @mouseover="blogDropdown = true">
+                        <NuxtLink  to="/">
                             Angus Gaukroger
                         </NuxtLink>
                     </h2>
@@ -11,16 +11,18 @@
                 
                 <div>
                     <ul class=" hidden md:flex flex-row justify-between items-center gap-8 font-semibold">
-                        <NuxtLink to="/" class="hover:text-mine-shaft-300 hover:cursor-pointer">Home</NuxtLink >
-                        <NuxtLink to="/#about" class="hover:text-mine-shaft-300 hover:cursor-pointer">About</NuxtLink >
-                        <NuxtLink to="/#blog" class="hover:text-mine-shaft-300 hover:cursor-pointer">Blog</NuxtLink >
-                        <NuxtLink to="/#projects" class="hover:text-mine-shaft-300 hover:cursor-pointer">Projects</NuxtLink >
-                        <NuxtLink to="/#contact" class="hover:text-mine-shaft-300 hover:cursor-pointer">Contact</NuxtLink >
+                        <NuxtLink @mouseover="blogDropdown = true" to="/" class="hover:text-mine-shaft-300 hover:cursor-pointer">Home</NuxtLink >
+                        <NuxtLink @mouseover="blogDropdown = true" to="/#about" class="hover:text-mine-shaft-300 hover:cursor-pointer">About</NuxtLink >
+             
+                        <NuxtLink @mouseover="blogDropdown = false" to="/#blog"   class=" hover:text-mine-shaft-300 hover:cursor-pointer">Blog</NuxtLink >
+               
+                        <NuxtLink @mouseover="blogDropdown = true" to="/#projects" class="hover:text-mine-shaft-300 hover:cursor-pointer">Projects</NuxtLink >
+                        <NuxtLink @mouseover="blogDropdown = true" to="/#contact" class="hover:text-mine-shaft-300 hover:cursor-pointer">Contact</NuxtLink >
                     </ul>
                      
 
                 </div>
-                <a href="https://connectdorset.com" class="hidden md:flex  bg-mine-shaft-400 hover:bg-mine-shaft-300 hover:cursor-pointer px-4 py-2 rounded-xl">
+                <a @mouseover="blogDropdown = true" href="https://connectdorset.com" class="hidden md:flex  bg-mine-shaft-400 hover:bg-mine-shaft-300 hover:cursor-pointer px-4 py-2 rounded-xl">
                    Connect Dorset
                 </a>
                 <button @click="navState = !navState" class="flex md:hidden text-mine-shaft-100">
@@ -37,6 +39,15 @@
                 
                 
             </nav>
+            <!-- Dropdown -->
+            <div   :class="blogDropdown ? 'hidden' : 'flex top-20'" class="fixed top-0 w-full flex flex-row justify-center   text-soft-peach-100  h-2/6  z-[100]">
+                <ul @mouseover="blogDropdown=false" @mouseleave="blogDropdown=true" class="w-24 px-2 h-full flex flex-col items-center justify-evenly bg-mine-shaft-600 border-soft-peach-100   border-l-2 border-b-2 border-r-2 rounded-b-xl">
+                    <NuxtLink @click="blogDropdown=true" to="/blog/travel" class="hover:text-mine-shaft-300 hover:cursor-pointer">Travel</NuxtLink >
+                    <NuxtLink @click="blogDropdown=true" to="/blog/lifestyle" class="hover:text-mine-shaft-300 hover:cursor-pointer">Lifestyle</NuxtLink >
+                    <NuxtLink @click="blogDropdown=true" to="/blog/software" class="hover:text-mine-shaft-300 hover:cursor-pointer">Software</NuxtLink >
+                    <NuxtLink @click="blogDropdown=true" to="/blog/fitness" class="hover:text-mine-shaft-300 hover:cursor-pointer">Fitness</NuxtLink >
+                </ul>
+            </div>
             <div class="h-screen w-full md:hidden flex flex-col items-center">
                 <ul :class="navState ? 'flex' : 'hidden'" class="mt-20 py-4 text-soft-peach-100 h-1/2 w-full fixed z-[99] md:hidden flex-col bg-mine-shaft-600 justify-between items-center gap-2 font-semibold">
                     <NuxtLink to="/" class=" hover:text-mine-shaft-300 hover:cursor-pointer"><a @click="navState = !navState">Home</a></NuxtLink >
@@ -56,11 +67,8 @@
 </template>
 
 <script setup>
-    // import {SlideUpDown} from 'vue-slide-up-down';
     var navState = ref(false);
+    var blogDropdown = ref(true);
 
-    // function toggleNav() {
-        
-    //     console.log(navState)
-    // }
+
 </script>
--- pages/blog/travel/index.vue
+++ pages/blog/travel/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="h-screen w-full text-black flex flex-col items-center justify-center">
         Welcome to the travel blog section.
     </div>
 </template>
