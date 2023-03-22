eb2d97cedd73ce89f3ce16a46de1f92aa6a9de4b
Angus Gaukroger <angusgaukroger@looptechnology.com>
2023-03-22 11:25:21 +0000

    added navbar image, shifted around stuff

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,15 +1,16 @@
 <template>
     <nav class="w-full overflow-x-hidden z-[99] top-0 fixed h-20 py-3 px-6 bg-gradient-to-tr from-mine-shaft-600 to-mine-shaft-500 text-soft-peach-100 flex flex-row justify-around items-center">
-                <div class="flex flex-col justify-center items-center h-full">
-                    <h2 class="font-semibold text-xl " @mouseover="blogDropdown = true">
-                        <NuxtLink  to="/">
+                <NuxtLink to="/#home" class="flex flex-row justify-center items-center h-full gap-2 ">
+                    <img class="w-9 scale-100 border-2 border-soft-peach-100 rounded-full" src="https://store.brth.uk/angus-port/bogota.jpg" />
+                    <h2 class="font-semibold text-lg " @mouseover="blogDropdown = true">
+                       
                             Angus Gaukroger
-                        </NuxtLink>
+                        
                     </h2>
                     <!-- <h3 class="text-xs">Software Developer</h3> -->
-                </div>
+                </NuxtLink>
                 
-                <div>
+                <div class="mr-8">
                     <ul class=" hidden md:flex flex-row justify-between items-center gap-8 font-semibold">
                         <NuxtLink @mouseover="blogDropdown = true" to="/#home" class="hover:text-mine-shaft-300 hover:cursor-pointer">Home</NuxtLink >
                         <NuxtLink @mouseover="blogDropdown = true" to="/#about" class="hover:text-mine-shaft-300 hover:cursor-pointer">About</NuxtLink >
@@ -25,7 +26,7 @@
                 <a @mouseover="blogDropdown = true" href="https://connectdorset.com" class="hidden md:flex  bg-mine-shaft-400 hover:bg-mine-shaft-300 hover:cursor-pointer px-4 py-2 rounded-xl">
                    Connect Dorset
                 </a>
-                <button @click="navState = !navState" class="flex md:hidden text-mine-shaft-100">
+                <button @click="navState = !navState" class="flex md:hidden text-mine-shaft-100 mr-7">
                     <svg :class="navState ? 'hidden' : 'flex'" xmlns="http://www.w3.org/2000/svg" class=" h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="#e6e1e3"
                     stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
