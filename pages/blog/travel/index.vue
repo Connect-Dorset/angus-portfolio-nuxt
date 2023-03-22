4eecec164c720615d97533cf981b539717994b5f
Angus Gaukroger <angusgaukroger@looptechnology.com>
2023-03-22 14:59:39 +0000

    blog section structure on big screens

--- pages/blog/travel/index.vue
+++ pages/blog/travel/index.vue
@@ -1,7 +1,97 @@
 <template>
-    <div class="h-screen w-full text-black flex flex-col items-center justify-center">
-        Welcome to the travel blog section.
-    </div>
+    <div class="w-full h-auto flex flex-col justify-start items-center gap-3 px-5 pt-24 pb-6">
+        <h1 class="text-4xl text-mine-shaft-800">Travel Posts</h1>
+        <div class="mt-3 w-full h-full flex flex-col justify-start items-center gap-4">
+            <!-- Make Dynamic -->
+            <div class="w-7/12 h-52 grid grid-cols-3 grid-rows-1 rounded-xl bg-mine-shaft-100 gap-y-3" >
+                <!-- <div class="cols-span-1 aspect-square object-cover " style="background: url('https://store.brth.uk/angus-port/HorseRidingSalento.jpg') 50% 50% no-repeat;"></div> -->
+                <img  class="w-52 h-52 rounded-l-xl object-cover align-self-start  aspect-square" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg"/>
+                <div class="col-span-2 flex flex-col justify-center items-start text-mine-shaft-800 gap-2 px-3 py-2">
+                    <h1 class="text-2xl font-bold">Salento: Colombia's hidden gem</h1>
+                    <p>Lorem ipsum  cum. Ex officia excepturi rem accusantium debitis cupiditate doloremque iure sint ratione earum. Sint, id?</p>
+                    <div class="w-full flex flex-row gap-3 items-center">
+                        <div class="text-sm lg:text-md">
+                            <strong>Category: </strong>Travel
+                          </div>
+                          <div class="text-sm lg:text-md">
+                            <strong>Date: </strong>06/2022
+                          </div>
+                      </div>
+                      
+                </div>
+           </div>
+            <div class="w-7/12 h-52 grid grid-cols-3 grid-rows-1 rounded-xl bg-mine-shaft-100 gap-y-3" >
+                <!-- <div class="cols-span-1 aspect-square object-cover " style="background: url('https://store.brth.uk/angus-port/HorseRidingSalento.jpg') 50% 50% no-repeat;"></div> -->
+                <img  class="w-52 h-52 rounded-l-xl object-cover align-self-start  aspect-square" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg"/>
+                <div class="col-span-2 flex flex-col justify-center items-start text-mine-shaft-800 gap-2 px-3 py-2">
+                    <h1 class="text-2xl font-bold">Salento: Colombia's hidden gem</h1>
+                    <p>Lorem ipsum  cum. Ex officia excepturi rem accusantium debitis cupiditate doloremque iure sint ratione earum. Sint, id?</p>
+                    <div class="w-full flex flex-row gap-3 items-center">
+                        <div class="text-sm lg:text-md">
+                            <strong>Category: </strong>Travel
+                          </div>
+                          <div class="text-sm lg:text-md">
+                            <strong>Date: </strong>06/2022
+                          </div>
+                      </div>
+                      
+                </div>
+           </div>
+            <div class="w-7/12 h-52 grid grid-cols-3 grid-rows-1 rounded-xl bg-mine-shaft-100 gap-y-3" >
+                <!-- <div class="cols-span-1 aspect-square object-cover " style="background: url('https://store.brth.uk/angus-port/HorseRidingSalento.jpg') 50% 50% no-repeat;"></div> -->
+                <img  class="w-52 h-52 rounded-l-xl object-cover align-self-start  aspect-square" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg"/>
+                <div class="col-span-2 flex flex-col justify-center items-start text-mine-shaft-800 gap-2 px-3 py-2">
+                    <h1 class="text-2xl font-bold">Salento: Colombia's hidden gem</h1>
+                    <p>Lorem ipsum  cum. Ex officia excepturi rem accusantium debitis cupiditate doloremque iure sint ratione earum. Sint, id?</p>
+                    <div class="w-full flex flex-row gap-3 items-center">
+                        <div class="text-sm lg:text-md">
+                            <strong>Category: </strong>Travel
+                          </div>
+                          <div class="text-sm lg:text-md">
+                            <strong>Date: </strong>06/2022
+                          </div>
+                      </div>
+                      
+                </div>
+           </div>
+            <div class="w-7/12 h-52 grid grid-cols-3 grid-rows-1 rounded-xl bg-mine-shaft-100 gap-y-3" >
+                <!-- <div class="cols-span-1 aspect-square object-cover " style="background: url('https://store.brth.uk/angus-port/HorseRidingSalento.jpg') 50% 50% no-repeat;"></div> -->
+                <img  class="w-52 h-52 rounded-l-xl object-cover align-self-start  aspect-square" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg"/>
+                <div class="col-span-2 flex flex-col justify-center items-start text-mine-shaft-800 gap-2 px-3 py-2">
+                    <h1 class="text-2xl font-bold">Salento: Colombia's hidden gem</h1>
+                    <p>Lorem ipsum  cum. Ex officia excepturi rem accusantium debitis cupiditate doloremque iure sint ratione earum. Sint, id?</p>
+                    <div class="w-full flex flex-row gap-3 items-center">
+                        <div class="text-sm lg:text-md">
+                            <strong>Category: </strong>Travel
+                          </div>
+                          <div class="text-sm lg:text-md">
+                            <strong>Date: </strong>06/2022
+                          </div>
+                      </div>
+                      
+                </div>
+           </div>
+            <div class="w-7/12 h-52 grid grid-cols-3 grid-rows-1 rounded-xl bg-mine-shaft-100 gap-y-3" >
+                <!-- <div class="cols-span-1 aspect-square object-cover " style="background: url('https://store.brth.uk/angus-port/HorseRidingSalento.jpg') 50% 50% no-repeat;"></div> -->
+                <img  class="w-52 h-52 rounded-l-xl object-cover align-self-start  aspect-square" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg"/>
+                <div class="col-span-2 flex flex-col justify-center items-start text-mine-shaft-800 gap-2 px-3 py-2">
+                    <h1 class="text-2xl font-bold">Salento: Colombia's hidden gem</h1>
+                    <p>Lorem ipsum  cum. Ex officia excepturi rem accusantium debitis cupiditate doloremque iure sint ratione earum. Sint, id?</p>
+                    <div class="w-full flex flex-row gap-3 items-center">
+                        <div class="text-sm lg:text-md">
+                            <strong>Category: </strong>Travel
+                          </div>
+                          <div class="text-sm lg:text-md">
+                            <strong>Date: </strong>06/2022
+                          </div>
+                      </div>
+                      
+                </div>
+           </div>
+        </div>
+        
+ 
+     </div>
 </template>
 
 <script setup>
--- pages/blog/travel/stories/index.vue
+++ pages/blog/travel/stories/index.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="w-full h-screen flex flex-col justify-center items-center">
-       
-    </div>
+    <div class="w-full h-full flex flex-col justify-center items-center px-3">
+        <h1>Articles</h1>
+        <div class="w-10/12 grid grid-cols-4 ">
+             <img class="cols-span-1 aspect-square w-full" src="https://store.brth.uk/angus-port/HorseRidingSalento.jpg" />
+             <div class="cols col-span-3">
+ 
+             </div>
+        </div>
+ 
+     </div>
 </template>
 
 <script setup>
